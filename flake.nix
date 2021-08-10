9624d6bea08a7b755f2958774087bb42ef80c704
Mazurel <mateusz.mazur@yahoo.com>
2021-08-10 10:03:45 +0200

    Modernized flake.nix
    - Added development shell
    - Improved overlay for other flakes to use
    - Added packages that are part of default.nix to the `packages` field
    - Removed checks, as `nix flake check` automatically checks `packages`

--- flake.nix
+++ flake.nix
@@ -1,21 +1,35 @@
 {
   description = "Build NPM packages in Nix and lightweight NPM registry";
+  inputs.nixpkgs.url = "nixpkgs/master";
 
-  outputs = { self, nixpkgs }: let
-    systems = [ "i686-linux" "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
-  in {
-    overlay = final: prev: {
-      napalm = {
-        inherit (import ./. { pkgs = final; })
-          buildPackage snapshotFromPackageLockJson;
-      };
+  outputs = { self, nixpkgs }:
+    let
+      # List of systems that are supported
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        });
+    in {
+      overlay = final: prev: { napalm = import ./. { pkgs = final; }; };
+
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system}.napalm)
+          hello-world hello-world-deps netlify-cli deckdeckgo-starter
+          bitwarden-cli napalm-registry;
+      });
+
+      devShell = forAllSystems
+        (system: nixpkgsFor.${system}.napalm.napalm-registry-devshell);
+
+      
     };
-
-    checks = nixpkgs.lib.genAttrs systems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in removeAttrs (import ./. { inherit pkgs; }) [
-      "buildPackage" "napalm-registry" "napalm-registry-devshell"
-      "snapshotFromPackageLockJson"
-    ]);
-  };
 }
