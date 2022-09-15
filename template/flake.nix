19278c4ce037a2dd286437a909467c25f2b0349f
David Houston <houstdav000@gmail.com>
2022-09-15 14:01:05 -0400

    template: Clean up outputs
    Clean up the template functions and outputs using the legacyPackages
    napalm output and an eta reduction.

--- template/flake.nix
+++ template/flake.nix
@@ -7,46 +7,36 @@
   # Import napalm
   inputs.napalm.url = "github:nix-community/napalm";
 
+  # Configuring "follows" lets you configure the source for the build environment.
+  inputs.napalm.inputs.nixpkgs.follows = "nixpkgs";
+
   outputs = { self, nixpkgs, napalm }:
     let
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 self.lastModifiedDate;
-
       # System types to support.
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system:
-        import nixpkgs {
-          inherit system;
-          # Add napalm to you overlay's list
-          overlays = [
-            self.overlay
-            napalm.overlay
-          ];
-        });
-
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
       # A Nixpkgs overlay.
-      overlay = final: prev: {
-        # Example package
-        hello-world = final.napalm.buildPackage ./hello-world { };
-      };
+      overlay = final: prev:
+        let
+          napalmPkg = napalm.legacyPackages."${final.stdenv.hostPlatform.system}";
+        in
+        {
+          hello-world = napalmPkg.buildPackage ./hello-world { };
+        };
 
       # Provide your packages for selected system types.
       packages = forAllSystems (system: {
-        inherit (nixpkgsFor.${system}) hello-world;
+        hello-world = napalm.legacyPackages."${system}".buildPackage ./hello-world { };
       });
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
       # package.
       defaultPackage =
-        forAllSystems (system: self.packages.${system}.hello-world);
+        forAllSystems (system: self.packages."${system}".hello-world);
     };
 }
